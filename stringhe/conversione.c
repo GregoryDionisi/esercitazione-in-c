65c3707d9ab3a57be1e1ac2e8b1eb97f0fad8ffb
GregoryDionisi <gregoryd324@gmail.com>
2026-01-06 18:30:59 +0100

    Inizio

--- stringhe/conversione.c
+++ stringhe/conversione.c
@@ -10,7 +10,7 @@ int main(){
     gets(s); //RICORDATI che è necessario solamente il nome dell'array e non anche l'opertore di indicizzazione []
 
     for(i = 0; s[i] != 0; i++){
-        if(s[i] != ' ' && s[i] >= 'a' && s[i] <= 'z'){ //RICORDATI di saltare lo spazio visto che è anch'esso un carattere e NON ha un maiuscolo giustamente
+        if(s[i] >= 'a' && s[i] <= 'z'){ //RICORDATI di saltare lo spazio visto che è anch'esso un carattere e NON ha un maiuscolo giustamente
             s[i] = s[i] + ('A' - 'a');
         }
     }
